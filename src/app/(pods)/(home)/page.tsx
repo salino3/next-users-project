cf236614351cb22a7073e75a4f3cae9cc1ce9772
Salino3 <flaviobcn012@gmail.com>
2026-05-19 09:57:01 +0200

    fix

--- src/app/(pods)/(home)/page.tsx
+++ src/app/(pods)/(home)/page.tsx
@@ -5,8 +5,8 @@ import { useShallow } from "zustand/shallow";
 import { getUsersList } from "@/app/hooks/get-fetch-users";
 import { useAppStore } from "@/app/store/provider";
 import { UserList } from "@/app/components/users-list";
-import "./home.styles.scss";
 import { LoginForm } from "@/app/components/login-form/login-form-component";
+import "./home.styles.scss";
 
 export default function HomePage() {
   const { users, currentUser, getUsers, clearUser } = useAppStore(
