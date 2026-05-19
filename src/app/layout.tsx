bb4601238bc2af2da31ad284991fb282de5603b2
Salino3 <flaviobcn012@gmail.com>
2026-05-19 10:06:51 +0200

    add comment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.scss";
 
+// enviroment variable client: NEXT_PUBLIC_*
+// enviroment variable server: *
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
